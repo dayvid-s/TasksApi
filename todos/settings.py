a77ed62314ee09d7a0abde4eee7ee151dfbfdc97
Dayvid <dayvid7santos2002@outlook.com>
2023-03-11 17:50:05 -0300

    changed to SQL database

--- todos/settings.py
+++ todos/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolistdatabase',
+        'PORT': '3306',
+        'HOST': 'localhost',
+        'USER': 'Dayvid',
+        'PASSWORD': '083a609da@',
     }
 }
 
